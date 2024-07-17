dc1962be78f032777985736d77993cee68fb329c
window1 <doserack@gmail.com>
2024-07-17 10:47:08 +0900

    Initial Commit

---
+++ NavigationEffetct/NavigationEffetctApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationEffetctApp.swift
+//  NavigationEffetct
+//
+//  Created by window1 on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationEffetctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
